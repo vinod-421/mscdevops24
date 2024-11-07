e8fc56fa1f03db7c23c9d64e3823229586269eb7
“vinod-421” <l00187421@atu.ie>
2024-11-07 21:49:06 +0000

    code(mscdevops24) updated  file #2

--- demo/src/main/java/com/atu/demo/DemoApplication.java
+++ demo/src/main/java/com/atu/demo/DemoApplication.java
@@ -9,5 +9,10 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
+	
+		private DemoApplication() {
+			// Prevent instantiation
+		}
+	
+	
 }
