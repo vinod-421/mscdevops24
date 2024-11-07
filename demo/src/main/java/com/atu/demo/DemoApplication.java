37a3611a8fd1b1d36a3f0324a1df67072ae7eb69
“vinod-421” <l00187421@atu.ie>
2024-11-07 22:00:38 +0000

    code(mscdevops24) updated file #5

--- demo/src/main/java/com/atu/demo/DemoApplication.java
+++ demo/src/main/java/com/atu/demo/DemoApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public final class DemoApplication {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	
